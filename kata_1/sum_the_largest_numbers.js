bdc3cdb1542be2e715d55117d9f9d4589b662cbc
Harp Sandhu <happacode@protonmail.com>
2021-09-06 20:56:40 -0700

    function console.log and expected output

--- kata_1/sum_the_largest_numbers.js
+++ kata_1/sum_the_largest_numbers.js
@@ -2,3 +2,7 @@
 const sumLargestNumbers = function(data) {
 
 }
+
+console.log(sumLargestNumbers([1, 10])); // Answer = 11
+console.log(sumLargestNumbers([1, 2, 3])); // Answer = 5
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); // Answer = 126
