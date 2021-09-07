2779e74f41045dcf07d5bb2d4c9c30df9a14000b
Harp Sandhu <happacode@protonmail.com>
2021-09-06 20:18:46 -0700

    function name

--- kata_1/sum_the_largest_numbers.js
+++ kata_1/sum_the_largest_numbers.js
@@ -1,1 +1,4 @@
 // In this exercise, we will be given an array of 2 or more numbers. We will then have to find the two largest numbers in that array, and sum them together.
+const sumLargestNumbers = function(data) {
+
+}
