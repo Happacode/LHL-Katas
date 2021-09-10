c0e687d99840a8ef1ca7708396e61426c9ede353
Harp Sandhu <happacode@protonmail.com>
2021-09-09 20:52:38 -0700

    function Output and Solution

--- kata_3/number_of_vowels.js
+++ kata_3/number_of_vowels.js
@@ -1,7 +1,15 @@
 const numberOfVowels = function(data) {
-  
+  let count = 0;
+  let vowels = ("aeiouAEIOU");
+
+  for (let i = 0; i < data.length; i++) {
+    if (vowels.indexOf(data[i]) > (-1)) {
+      count++;
+    }
+  }
+  return count;
 };
 
-console.log(numberOfVowels("orange"));
-console.log(numberOfVowels("lighthouse labs"));
-console.log(numberOfVowels("aeiou"));
+console.log(numberOfVowels("orange")); // Answer 3
+console.log(numberOfVowels("lighthouse labs")); // Answer 5
+console.log(numberOfVowels("aeiou")); // Answer 5
