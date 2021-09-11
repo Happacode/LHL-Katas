586d3c41cca147fb1e4d690364ea2885b6624b62
Harp Sandhu <happacode@protonmail.com>
2021-09-10 23:23:34 -0700

    function solution and output

--- kata_4/instructor_with_longest_name.js
+++ kata_4/instructor_with_longest_name.js
@@ -1,5 +1,16 @@
 const instructorWithLongestName = function(instructors) {
+  let longestName = 0;
+  let countedName = 0;
+  let result;
   
+  for(let instructor of instructors){
+    countedName = instructor.name.length;
+    if(countedName > longestName){
+      longestName = countedName;
+      result = instructor;
+    }
+  }
+  return result;
 };
 
 console.log(instructorWithLongestName([
@@ -7,9 +18,9 @@ console.log(instructorWithLongestName([
   {name: "Jeremiah", course: "Web"},
   {name: "Ophilia", course: "Web"},
   {name: "Donald", course: "Web"}
-]));
+])); // Answer 'Jeremiah', course 'Web'
 console.log(instructorWithLongestName([
   {name: "Matthew", course: "Web"},
   {name: "David", course: "iOS"},
   {name: "Domascus", course: "Web"}
-]));
+])); // Answer 'Domascus', course: 'Web'
