f080d4e3f7d57c988d2f2d8cc5425c805df791b3
Harp Sandhu <happacode@protonmail.com>
2021-09-10 23:20:55 -0700

    function console.log

--- kata_4/instructor_with_longest_name.js
+++ kata_4/instructor_with_longest_name.js
@@ -1,3 +1,15 @@
 const instructorWithLongestName = function(instructors) {
   
 };
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
