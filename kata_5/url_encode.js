0509f0e98359babd9b7a5cdc3d9182370239ec0c
Harp Sandhu <happacode@protonmail.com>
2021-09-11 23:35:45 -0700

    function solution and output

--- kata_5/url_encode.js
+++ kata_5/url_encode.js
@@ -1,7 +1,12 @@
 const urlEncode = function(text) {
 
+  for (let i = 0; i < text.length; i++) {
+    text = text.trim().split(" ").join("%20");
+  }
+  return text;
+
 };
 
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
+console.log(urlEncode("Lighthouse Labs")); // Answer Lighthouse%20Labs
+console.log(urlEncode(" Lighthouse Labs ")); // Answer Lighthouse%20Labs
+console.log(urlEncode("blue is greener than purple for sure")); // Answer blue%20is%20greener%20than%20purple%20for%20sure
