0aa6bcc10c7d52c8f9afe4f4392fb393d8e36197
Harp Sandhu <happacode@protonmail.com>
2021-09-16 21:35:16 -0700

    function console.log

--- kata_9/case_maker.js
+++ kata_9/case_maker.js
@@ -1,3 +1,7 @@
 let camelCase = function(str) {
 
 };
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
